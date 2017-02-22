362a87721493c46a577d5f156683b10efbd0b130
Maxime Daniel <root@maxux.net>
2017-02-22 16:02:03 +0100

    update wifi dongle hostapd to 2.6

--- wifi-special-dongle/setup.sh
+++ wifi-special-dongle/setup.sh
@@ -8,14 +8,14 @@ apt-get install -y rtl8192eu-dkms
 mkdir /opt/netpoc
 cd /opt/netpoc
 
-wget -c http://w1.fi/releases/hostapd-2.5.tar.gz
-rm -rfv hostapd-2.5/
-tar -xvf hostapd-2.5.tar.gz
+wget -c http://w1.fi/releases/hostapd-2.6.tar.gz
+rm -rfv hostapd-2.6/
+tar -xvf hostapd-2.6.tar.gz
 
 apt-get install -y libnl-3-dev libnl-genl-3-dev pkg-config libssl-dev
 git clone --depth=1 https://github.com/pritambaral/hostapd-rtl871xdrv.git
 
-cd /opt/netpoc/hostapd-2.5/hostapd
+cd /opt/netpoc/hostapd-2.6/hostapd
 cp defconfig .config
 
 sed -i s/'#CONFIG_LIBNL32=y'/'CONFIG_LIBNL32=y'/g .config
